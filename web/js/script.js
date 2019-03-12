181749bdbaec5fab6bb3c86615711df8cd590f1b
Adam Fister <arfister@gmail.com>
2019-03-12 13:00:29 -0400

    check win conditions

--- web/js/script.js
+++ web/js/script.js
@@ -1,140 +1,188 @@
-const container = document.querySelector('.container');
 
-// -----  PERSON FACTORY  -----
-const personFactory = (name, marker, playerNumber) => {
-  const displayPerson = () => {
-    document.querySelector(`#player${playerNumber}`).innerText = `${name}, ${marker}`
-  };
-
-  displayPerson();
-  
-  return { name, marker, displayPerson };
-};
-
-
-
-// -----  GAME FACTORY  -----
-const gameFactory = (player1, player2) => {
-  document.querySelector('.gameInformation').classList.remove('hidden');
-  document.querySelector('.getPlayerInfo').classList.add('hidden')
-
-  let currentPlayer = player1;
-  let gameArray = Array(9).fill('');
-
-  // Checks if all items in array are the same
-  Array.prototype.allSameValues = function() {
-    if (this[0] === "") { return false; }
-    for (let i = 1; i < this.length; i++) {
-      if (this[i] !== this[0]) { return false; }
-    } 
-    return true;
-  }
-
-  const validChoice = (target) => {
-    const targetIndex = target.id.slice(-1);
-    return (gameArray[targetIndex] === "") ? true : false;
-  }
-
-  const playerChoose = (currentPlayer, target) => {
-    const targetIndex = target.id.slice(-1);
-    const playerMarker = currentPlayer.marker;
-    if (gameArray[targetIndex] === "") {
-      gameArray[targetIndex] = playerMarker;
+function assignIndex(button) {
+    if (button == "00") {
+        let newButton = 0;
+        return newButton;
     }
-  }
-
-  const renderBoard = () => {
-    const boxes = document.querySelectorAll('.box')
-    boxes.forEach(function(box, index) {
-      box.innerHTML = `<p>${gameArray[index]}</p>`
-    })
-  };
-
-  const switchPlayer = (currentPlayer) => {
-    return (currentPlayer === player1) ? player2 : player1;
-  };
-
-  const checkForTie = (gameArray) => {
-    let tieGame = true;
-    gameArray.forEach(function(position) {
-      if (position === "") { tieGame = false; }
-    })
-    return tieGame;
-  }
-
-  const checkForWin = (gameArray) => {
-    let win = false;
-
-    const winningCombos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-
-    winningCombos.forEach(arr => {
-      const a = gameArray[arr[0]];
-      const b = gameArray[arr[1]];
-      const c = gameArray[arr[2]];
-      if ([a, b, c].allSameValues()) {
-        win = true;
-        return;
-      };
-    });
-
-    return win;
-  }
-
-  const gameOver = player => {
-    // If not passed ARG, tie game is displayed
-    const messageAnchor = document.querySelector(".message")
-    const gameInformation = document.querySelector('.gameInformation')
-
-    const changeScreen = (displayMessage) => {
-      container.style.display = 'none';
-      gameInformation.style.display = 'none';
-      messageAnchor.innerHTML = `<h1>${displayMessage}</h1>`
+    if (button == "01") {
+        let newButton = 1;
+        return newButton;
     }
+    if (button == "02") {
+        let newButton = 2;
+        return newButton;
+    }
+    if (button == "10") {
+        let newButton = 3;
+        return newButton;
+    }
+    if (button == "11") {
+        let newButton = 4;
+        return newButton;
+    }
+    if (button == "12") {
+        let newButton = 5;
+        return newButton;
+    }
+    if (button == "20") {
+        let newButton = 6;
+        return newButton;
+    }
+    if (button == "21") {
+        let newButton = 7;
+        return newButton;
+    }
+    if (button == "22") {
+        let newButton = 8;
+        return newButton;
+    }
+}
 
-    const tieGame = () => { changeScreen("Tie Game"); }
+function gameBoard(){
+    var str = "";
+    var obj = document.getElementById("board");
 
-    const winner = () => { changeScreen(`You Win ${player.name}!`); }
-    
-    player === undefined ? tieGame() : winner()
-  }
+    for (var i=0;i<3;i++) {
+        str += "<div class='row' id='test_row" + i + "'>";
+        var str2 = "";
 
-  container.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.className === "box") {
-      let valid = validChoice(target)
-      if (valid) {
-        playerChoose(currentPlayer, target);
-        renderBoard();
-        const tie = checkForTie(gameArray);
-        const win = checkForWin(gameArray, currentPlayer);
-        if (tie) {
-          gameOver();
-        } else if (win) {
-          gameOver(currentPlayer);
-        } else {
-          currentPlayer = switchPlayer(currentPlayer);
+        for (var j = 0; j<3;j++) {
+     //       var key = "index" + String(i) + String(j);
+            str2 += "<div class='col' id ='test_col'>";
+            var key2 = String(i) + String(j);
+            
+            str2 += "<button type='button' ";
+            str2 += "id='" + key2 + "' ";
+            
+            str2 += "class='btn btn-secondary' onclick=whoseTurn('" + key2 + "')> </button>";
+            str2 += "</div>";
         }
-      }
-    };
-  });
-};
+        
+        str += str2;
+        str += "</div>";
+        
+    }
+    
+    obj.innerHTML = str;
+    
+
+// clickable tiles
+    // unique id for each tile
+}
+
+var alreadyClicked = [];
+var count = 0;
+let arrWinner = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+function whoseTurn(v){
+    console.log(v);
 
 
+    var obj = document.getElementById(v);
+    
+    if ((count % 2 == 0) && (!alreadyClicked.includes(v))) {
+        
+        count++;
+        alreadyClicked.push(v);
+        let newButton = assignIndex(v);
+        obj.innerHTML = "X";
+        arrWinner[newButton] = 1;
+        checkWinner(arrWinner);
 
-// ----- INIT GAME -----
-document.querySelector('#startGame').addEventListener('click', function() {
-  const player1Name = document.querySelector('#player1Name').value
-  const player2Name = document.querySelector('#player2Name').value
-  let player1 = personFactory(player1Name, "X", 1);
-  let player2 = personFactory(player2Name, "O", 2);
-  gameFactory(player1, player2);
-})
+    } else if ((count % 2 == 1) && (!alreadyClicked.includes(v))) {
+        
+        count++;
+        alreadyClicked.push(v);
+        let newButton = assignIndex(v);
+        obj.innerHTML = "0";
+        arrWinner[newButton] = 2;
+        checkWinner(arrWinner);
+    }
+
+    function checkWinner(a) {
+        //Check rows
+        if ((a[0] + a[1] + a[2] == 3) && (a[0] != 0 && a[1] != 0 && a[2] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[0] + a[1] + a[2] == 6) && (a[0] != 0 && a[1] != 0 && a[2] != 0)) {
+          console.log("Winner is O!")
+        }
+       
+        if ((a[3] + a[4] + a[5] == 3) && (a[3] != 0 && a[4] != 0 && a[5] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[3] + a[4] + a[5] == 6) && (a[3] != 0 && a[4] != 0 && a[5] != 0)) {
+          console.log("Winner is O!")
+        }
+       
+        if ((a[6] + a[7] + a[8] == 3) && (a[6] != 0 && a[7] != 0 && a[8] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[6] + a[7] + a[8] == 6) && (a[6] != 0 && a[7] != 0 && a[8] != 0)) {
+          console.log("Winner is O!")
+        }
+       
+        //Check columns
+        if ((a[0] + a[3] + a[6] == 3) && (a[0] != 0 && a[3] != 0 && a[6] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[0] + a[3] + a[6] == 6) && (a[0] != 0 && a[3] != 0 && a[6] != 0)) {
+          console.log("Winner is O!")
+        }
+       
+        if ((a[1] + a[4] + a[7] == 3) && (a[1] != 0 && a[4] != 0 && a[7] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[1] + a[4] + a[7] == 6) && (a[1] != 0 && a[4] != 0 && a[7] != 0)) {
+          console.log("Winner is O!")
+        }
+       
+        if ((a[2] + a[5] + a[8] == 3) && (a[2] != 0 && a[5] != 0 && a[8] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[2] + a[5] + a[8] == 6) && (a[2] != 0 && a[5] != 0 && a[8] != 0)) {
+          console.log("Winner is O!")
+        }
+       
+        //Check diagonals
+        if ((a[0] + a[4] + a[8] == 3) && (a[0] != 0 && a[4] != 0 && a[8] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[0] + a[4] + a[8] == 6) && (a[0] != 0 && a[4] != 0 && a[8] != 0)) {
+          console.log("Winner is O!")
+        }
+       
+        if ((a[2] + a[4] + a[6] == 3) && (a[2] != 0 && a[4] != 0 && a[6] != 0)) {
+          console.log("Winner is X!")
+        }
+        if ((a[2] + a[4] + a[6] == 6) && (a[2] != 0 && a[4] != 0 && a[6] != 0)) {
+          console.log("Winner is O!")
+        }
+       }
+
+// x gets first move
+// increment counter to keep track of whose turn
+// counter even = x turn, counter odd = o turn
+// array keeping track of used tiles
+// cut off function to used tiles based on array
+// html element displaying current player
+}
+
+function printSymbol(){
+// take in unique id
+// insert html/img into row-column of unique id
+}
+
+function winner(){
+// check for rows of 3
+}
+
+function pageRefresh(){
+// reset game
+}
+
+function start(){  
+    console.log('At Start');
+
+    gameBoard();
+}
